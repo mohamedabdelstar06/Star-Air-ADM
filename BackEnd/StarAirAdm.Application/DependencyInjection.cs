5704ece4b98f39f2a1a00beb265838f0143676ec
Mohamed Abdelstar <mohamedabdelstar06@gmail.com>
2026-06-26 20:01:31 +0300

    refactor: create Application layer Dependency Injection

---
+++ BackEnd/StarAirAdm.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿namespace StarAirAdm.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            
+            // Register Pipeline Behaviors
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ExceptionBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(TransactionBehavior<,>));
+        });
+
+        return services;
+    }
+}
